6e1458677e4e3cd12867879e5d534051f339e344
Mohamed Hazem <mohazemwork@gmail.com>
2024-09-23 18:05:49 +0300

    Add project files.

---
+++ KoalaInventoryManagement/ViewModels/DashboardViewModel.cs
@@ -0,0 +1,8 @@
+﻿namespace KoalaInventoryManagement.ViewModels
+{
+    //here we will create the model to send to the view of dashboard which includes all
+    //info when we create the database isa
+    public class DashboardViewModel
+    {
+    }
+}
---
+++ KoalaInventoryManagement/wwwroot/js/site.js
@@ -0,0 +1,8 @@
+﻿//next part is for the sidebar
+$("#right").hide();
+$(".change-sidebar").on("click", function () {
+    $("#right").toggle();
+    $("#left").toggle();
+    $(".main").toggleClass("size-change")
+});
+
