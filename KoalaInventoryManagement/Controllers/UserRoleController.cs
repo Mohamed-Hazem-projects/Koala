40029633105f4bb94367d67988055268fffbb922
arsany155 <arsanynashaat@yahoo.com>
2024-10-08 00:14:02 +0300

    RolePage

---
+++ KoalaInventoryManagement/Controllers/UserRoleController.cs
@@ -0,0 +1,128 @@
+﻿using Inventory.Data.Models;
+using KoalaInventoryManagement.ViewModels;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Data;
+
+namespace KoalaInventoryManagement.Controllers
+{
+    public class UserRoleController : Controller
+    {
+        public UserManager<ApplicationUser> _UserManager;
+        public RoleManager<IdentityRole> _RoleManager;
+        public UserRoleController(UserManager<ApplicationUser> userManager , RoleManager<IdentityRole> roleManager)
+        {
+            _UserManager = userManager;
+            _RoleManager = roleManager;
+        }
+        public async Task<IActionResult> Index()
+        {
+            var users = await _UserManager.Users.ToListAsync();
+            var roles = await _RoleManager.Roles.ToListAsync(); 
+            var userRolesViewModel = new List<UserRolesViewModel>();
+
+            foreach (var user in users)
+            {
+                var userRoles = await _UserManager.GetRolesAsync(user);
+                userRolesViewModel.Add(new UserRolesViewModel
+                {
+                    UserId = user.Id,
+                    FullName = $"{user.FirstName} {user.LastName}",
+                    Roles = userRoles.ToList() 
+                });
+            }
+
+            ViewBag.Roles = roles;
+            return View(userRolesViewModel); 
+        }
+
+        public async Task<IActionResult> Delete(string id)
+        {
+            try
+            {
+                var user = await _UserManager.FindByIdAsync(id);
+                if (user == null)
+                {
+                    return NotFound();
+                }
+                var result = await _UserManager.DeleteAsync(user);
+
+                if (result.Succeeded)
+                {
+                    return RedirectToAction("Index");
+                }
+            }
+            catch (Exception ex)
+            {
+
+            }
+            return RedirectToAction("Index");
+        }
+            
+        public async Task<IActionResult> UserDetails(string Id)
+        {
+            var user = await _UserManager.FindByIdAsync(Id);
+
+            if (user == null)
+            {
+                return NotFound();
+            }
+            var userRoles = await _UserManager.GetRolesAsync(user);
+
+            var userDetailsViewModel = new UserDetailsViewModel
+            {
+                FirstName = user.FirstName,
+                LastName = user.LastName,
+                Email = user.Email,
+                phoneNumber = user.PhoneNumber,
+                roles = userRoles.ToList()
+            };
+
+            return View(userDetailsViewModel);
+        }
+
+        public async Task<IActionResult> UpdateUserRoles(string userId, List<string> Roles)
+        {
+            var appUser = await _UserManager.FindByIdAsync(userId);
+            if (appUser == null)
+            {
+                return NotFound();
+            }
+
+            var currentRoles = await _UserManager.GetRolesAsync(appUser);
+
+            foreach (var role in Roles)
+            {
+                if (!await _UserManager.IsInRoleAsync(appUser, role))
+                {
+                    var result = await _UserManager.AddToRoleAsync(appUser, role);
+                    if (!result.Succeeded)
+                    {
+                        return BadRequest($"Failed to add role {role} to user {appUser.UserName}");
+                    }
+                }
+            }
+
+            foreach (var role in currentRoles)
+            {
+                if (!Roles.Contains(role))
+                {
+                    var result = await _UserManager.RemoveFromRoleAsync(appUser, role);
+                    if (!result.Succeeded)
+                    {
+                        return BadRequest($"Failed to remove role {role} from user {appUser.UserName}");
+                    }
+                }
+            }
+            return RedirectToAction("Index");
+        }
+
+
+
+
+
+
+
+    }
+}
---
+++ KoalaInventoryManagement/ViewModels/Roles ViewModel/UserRolesViewModel.cs
@@ -0,0 +1,10 @@
+﻿namespace KoalaInventoryManagement.ViewModels
+{
+    public class UserRolesViewModel
+    {
+        public string UserId { get; set; }
+        public string FullName { get; set; }
+        public List<string> Roles { get; set; }
+    }
+
+}
