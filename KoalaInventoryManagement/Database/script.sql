f9e5735c8e82a6cc7a4728dd668aa0a742827259
YEG4 <ahmed.t.ghoneim@gmail.com>
2024-10-13 17:45:51 +0300

    Fixed number of items showon on the page

--- KoalaInventoryManagement/Database/script.sql
+++ KoalaInventoryManagement/Database/script.sql
@@ -1,8 +1,2 @@
-INSERT INTO "Sales" ("Id", "CreateAt", "ItemsSold", "ProductId", "SaleDate", "TotalPrice", "WareHouseId")
-VALUES (1, '2024-10-11 09:20:23.9539542', 5, 1, '2024-10-11 09:20:23.9539823', '100.0', 4);
-INSERT INTO "Sales" ("Id", "CreateAt", "ItemsSold", "ProductId", "SaleDate", "TotalPrice", "WareHouseId")
-VALUES (3, '2024-10-11 09:20:23.9541094', 15, 3, '2024-09-05 00:00:00', '300.0', 5);
-INSERT INTO "Sales" ("Id", "CreateAt", "ItemsSold", "ProductId", "SaleDate", "TotalPrice", "WareHouseId")
-VALUES (4, '2024-10-11 09:20:23.9541121', 20, 4, '2024-10-03 00:00:00', '400.0', 2);
-INSERT INTO "Sales" ("Id", "CreateAt", "ItemsSold", "ProductId", "SaleDate", "TotalPrice", "WareHouseId")
-VALUES (5, '2024-10-11 09:20:23.9541125', 25, 5, '2024-10-03 00:00:00', '500.0', 3);
+INSERT INTO Sales (CreateAt, ItemsSold, ProductId, SaleDate, TotalPrice, WareHouseId)
+VALUES ('2024-10-12 11:12:45.9541125', 10, 5, '2024-10-05', '150.0', 3);
