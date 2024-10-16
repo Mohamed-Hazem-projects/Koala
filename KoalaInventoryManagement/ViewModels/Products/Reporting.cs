c607b3507a31349155685f57fec5ef94e3ddfeff
Muhammedsalama5014 <midosalama5014@hotmail.com>
2024-10-16 22:57:44 +0300

    FirstCode

---
+++ KoalaInventoryManagement/ViewModels/Products/Reporting.cs
@@ -0,0 +1,11 @@
+﻿using KoalaInventoryManagement.Services;
+using Microsoft.AspNetCore.Mvc.Infrastructure;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace KoalaInventoryManagement.ViewModels.Products
+{
+    public class Reporting : PageModel
+    {
+      
+    }
+}
