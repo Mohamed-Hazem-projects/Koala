5efea73e664d81c727ecf2ccff4652c86647e6b3
Mohamed Hazem <mohazemwork@gmail.com>
2024-10-18 15:25:56 +0300

    added admin seed

--- Inventory.Data/Data Seeds/RolesSeed.cs
+++ Inventory.Data/Data Seeds/RolesSeed.cs
@@ -7,14 +7,14 @@ namespace Inventory.Data.Data_Seeds
     {
         public static List<IdentityRole> Roles { get; set; } = new List<IdentityRole>
         {
-            new IdentityRole{ Name = "Admin", NormalizedName = "ADMIN" },
-            new IdentityRole{ Name = "Staff", NormalizedName = "STAFF" },
-            new IdentityRole{ Name = "User", NormalizedName = "USER" },
-            new IdentityRole{ Name = "WHManager1", NormalizedName = "WHMANAGER1" }, 
-            new IdentityRole{ Name = "WHManager2", NormalizedName = "WHMANAGER2" }, 
-            new IdentityRole{ Name = "WHManager3", NormalizedName = "WHMANAGER3" }, 
-            new IdentityRole{ Name = "WHManager4", NormalizedName = "WHMANAGER4" }, 
-            new IdentityRole{ Name = "WHManager5", NormalizedName = "WHMANAGER5" }      
+            new IdentityRole{ Id = Guid.NewGuid().ToString(), Name = "Admin", NormalizedName = "ADMIN" },
+            new IdentityRole{ Id = Guid.NewGuid().ToString(), Name = "Staff", NormalizedName = "STAFF" },
+            new IdentityRole{ Id = Guid.NewGuid().ToString(), Name = "User", NormalizedName = "USER" },
+            new IdentityRole{ Id = Guid.NewGuid().ToString(), Name = "WHManager1", NormalizedName = "WHMANAGER1" },
+            new IdentityRole{ Id = Guid.NewGuid().ToString(), Name = "WHManager2", NormalizedName = "WHMANAGER2" },
+            new IdentityRole{ Id = Guid.NewGuid().ToString(), Name = "WHManager3", NormalizedName = "WHMANAGER3" },
+            new IdentityRole{ Id = Guid.NewGuid().ToString(), Name = "WHManager4", NormalizedName = "WHMANAGER4" },
+            new IdentityRole{ Id = Guid.NewGuid().ToString(), Name = "WHManager5", NormalizedName = "WHMANAGER5" }
         };
     }
 }
